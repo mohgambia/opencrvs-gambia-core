58e7d078b6d17fc4a0454003cab7edf1cd55ad6c
Alfonso <afoone@hotmail.com>
2021-02-20 09:56:38 +0000

    logs

--- create-folder-structure.sh
+++ create-folder-structure.sh
@@ -4,3 +4,7 @@ LOG_LOCATION=/var/log/opencrvs
 echo "Create log folder"
 sudo mkdir -p $LOG_LOCATION
 sudo chown -R $USER:$USER $LOG_LOCATION
+
+echo "create log for rotate"
+sudo touch $LOG_LOCATION/rotate-logs.log
+
